2ea0ccc1731c12b81bef6816d23703d6afa43c8b
germanrcb1105 <germanrcb@hotmail.com>
2022-03-09 00:03:25 -0500

    Agregar archivos de proyecto.

---
+++ CNTSIH/Controllers/CatalogoController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CNTSIH.Models;
+
+namespace CNTSIH.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        private CSIHEntities db = new CSIHEntities();
+
+        // GET: Catalogo
+        public async Task<ActionResult> Index()
+        {
+            var catalogo = db.Catalogo.Include(c => c.mCatalogo);
+            return View(await catalogo.ToListAsync());
+        }
+
+        // GET: Catalogo/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Catalogo catalogo = await db.Catalogo.FindAsync(id);
+            if (catalogo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(catalogo);
+        }
+
+        // GET: Catalogo/Create
+        public ActionResult Create()
+        {
+            ViewBag.mcatalogo_id = new SelectList(db.mCatalogo, "mcatalogo_id", "catalogo");
+            return View();
+        }
+
+        // POST: Catalogo/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "catalogo_id,mcatalogo_id,nombre,descripcion,estado")] Catalogo catalogo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Catalogo.Add(catalogo);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.mcatalogo_id = new SelectList(db.mCatalogo, "mcatalogo_id", "catalogo", catalogo.mcatalogo_id);
+            return View(catalogo);
+        }
+
+        // GET: Catalogo/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Catalogo catalogo = await db.Catalogo.FindAsync(id);
+            if (catalogo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.mcatalogo_id = new SelectList(db.mCatalogo, "mcatalogo_id", "catalogo", catalogo.mcatalogo_id);
+            return View(catalogo);
+        }
+
+        // POST: Catalogo/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "catalogo_id,mcatalogo_id,nombre,descripcion,estado")] Catalogo catalogo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(catalogo).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.mcatalogo_id = new SelectList(db.mCatalogo, "mcatalogo_id", "catalogo", catalogo.mcatalogo_id);
+            return View(catalogo);
+        }
+
+        // GET: Catalogo/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Catalogo catalogo = await db.Catalogo.FindAsync(id);
+            if (catalogo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(catalogo);
+        }
+
+        // POST: Catalogo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Catalogo catalogo = await db.Catalogo.FindAsync(id);
+            db.Catalogo.Remove(catalogo);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CNTSIH/Controllers/mCatalogoController.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CNTSIH.Models;
+
+namespace CNTSIH.Controllers
+{
+    public class mCatalogoController : Controller
+    {
+        private CSIHEntities db = new CSIHEntities();
+
+        // GET: mCatalogo
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.mCatalogo.ToListAsync());
+        }
+
+        // GET: mCatalogo/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            mCatalogo mCatalogo = await db.mCatalogo.FindAsync(id);
+            if (mCatalogo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mCatalogo);
+        }
+
+        // GET: mCatalogo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: mCatalogo/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "mcatalogo_id,catalogo,estado")] mCatalogo mCatalogo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.mCatalogo.Add(mCatalogo);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(mCatalogo);
+        }
+
+        // GET: mCatalogo/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            mCatalogo mCatalogo = await db.mCatalogo.FindAsync(id);
+            if (mCatalogo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mCatalogo);
+        }
+
+        // POST: mCatalogo/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "mcatalogo_id,catalogo,estado")] mCatalogo mCatalogo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mCatalogo).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(mCatalogo);
+        }
+
+        // GET: mCatalogo/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            mCatalogo mCatalogo = await db.mCatalogo.FindAsync(id);
+            if (mCatalogo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mCatalogo);
+        }
+
+        // POST: mCatalogo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            mCatalogo mCatalogo = await db.mCatalogo.FindAsync(id);
+            db.mCatalogo.Remove(mCatalogo);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+        public async Task<ActionResult> CreateCatalogo(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            mCatalogo mCatalogo = await db.mCatalogo.FindAsync(id);
+            if (mCatalogo == null)
+            {
+                return HttpNotFound();
+            }
+            var view = new Catalogo { mcatalogo_id = mCatalogo.mcatalogo_id, };
+             return View(view);
+        }
+
+        // POST: Catalogo/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateCatalogo( Catalogo catalogo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Catalogo.Add(catalogo);
+                await db.SaveChangesAsync();
+                return RedirectToAction(String.Format("Details/{0}", catalogo.mcatalogo_id));
+            }
+
+            //ViewBag.mcatalogo_id = new SelectList(db.mCatalogo, "mcatalogo_id", "catalogo", catalogo.mcatalogo_id);
+            return View(catalogo);
+        }
+
+        public async Task<ActionResult> EditCatalogo(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Catalogo catalogo = await db.Catalogo.FindAsync(id);
+            if (catalogo == null)
+            {
+                return HttpNotFound();
+            }
+            //ViewBag.mcatalogo_id = new SelectList(db.mCatalogo, "mcatalogo_id", "catalogo", catalogo.mcatalogo_id);
+            return View(catalogo);
+        }
+
+        // POST: Catalogo/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditCatalogo(Catalogo catalogo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(catalogo).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction(String.Format("Details/{0}", catalogo.mcatalogo_id));
+            }
+            //ViewBag.mcatalogo_id = new SelectList(db.mCatalogo, "mcatalogo_id", "catalogo", catalogo.mcatalogo_id);
+            return View(catalogo);
+        }
+
+        public async Task<ActionResult> DeleteCatalogo(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Catalogo catalogo = await db.Catalogo.FindAsync(id);
+            if (catalogo == null)
+            {
+                return HttpNotFound();
+            }
+            catalogo.estado = false;
+            db.Entry(catalogo).State = EntityState.Modified;
+            await db.SaveChangesAsync();
+            return RedirectToAction(String.Format("Details/{0}", catalogo.mcatalogo_id));
+            //ViewBag.mcatalogo_id = new SelectList(db.mCatalogo, "mcatalogo_id", "catalogo", catalogo.mcatalogo_id);
+            //return View(catalogo);
+        }
+    }
+}
